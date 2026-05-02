7525bffaac6878298a22b97441fa4df52ebbf907
Belkacem BENADDA <67480079+bekaben@users.noreply.github.com>
2026-05-02 07:55:32 +0100

    Rename module from tt_BFD100_Logic to tt_um_BFD100_Logic

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_BFD100_Logic (
+module tt_um_BFD100_Logic (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
